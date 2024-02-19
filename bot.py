ccc9a799f794bcb03abf83eb1c3ec18a7a8dc4df
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-02-19 21:14:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,28 @@
 import logging
-import logging.config
-from pyrogram import Client 
-from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+import os
 
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+if bool(os.environ.get("WEBHOOK", False)):
+from config import Config
+
+import pyrogram
 
 
-class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
-            name="renamer",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
-
-    async def start(self):
-       await super().start()
-       me = await self.get_me()
-       self.mention = me.mention
-       self.username = me.username 
-       self.force_channel = FORCE_SUB
-       if FORCE_SUB:
-         try:
-            link = await self.export_chat_invite_link(FORCE_SUB)                  
-            self.invitelink = link
-         except Exception as e:
-            logging.warning(e)
-            logging.warning("Make Sure Bot admin in force sub channel")             
-            self.force_channel = None      
-       logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
-      
-
-    async def stop(self, *args):
-      await super().stop()      
-      logging.info("Bot Stopped")
-        
-bot = Bot()
-bot.run()
+if __name__ == "__main__" :
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(
+        root="plugins"
+    )
+    app = pyrogram.Client(
+        "Rename-Pro",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.run()
